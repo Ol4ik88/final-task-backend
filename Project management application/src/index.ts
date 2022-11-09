7c8153684b538b3fa6781c0dff1eb1a4b55a0a19
yolf <gleoros@gmail.com>
2022-11-09 22:09:11 +0300

    feat: update config

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://user-113:OolM4UXzTrS76WjV@cluster0.dbcauyz.mongodb.net/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
