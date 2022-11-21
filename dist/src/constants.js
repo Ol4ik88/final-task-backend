767ebb0e3f5a41336f29cd54490e8bc1ed0b208c
yolf <gleoros@gmail.com>
2022-11-21 14:49:54 +0300

    dewploy

---
+++ dist/src/constants.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SECRET_KEY = exports.PORT = void 0;
+exports.PORT = 3000;
+// export const PORT = 4000;
+exports.SECRET_KEY = 'myVerySecretKey-04.05.2022';
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,8 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://user-113:OolM4UXzTrS76WjV@cluster0.dbcauyz.mongodb.net/?retryWrites=true&w=majority');
+        await mongoose.connect(process.env.DATABASE_URL || 'mongodb://root:secret@localhost/?retryWrites=true&w=majority');
+    // await mongoose.connect('mongodb://root:secret@localhost/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
